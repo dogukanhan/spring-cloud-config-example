e73b3f2bd5e23ddaeb36a0dd90e4120a63e76122
Doğukan HAN <dogukan.han1@kocssitem.com.tr>
2022-08-28 13:41:08 +0300

    Cloud config

---
+++ confserver/confserver/src/test/java/com/dogukanhan/sprconfex/confserver/ConfserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukanhan.sprconfex.confserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
